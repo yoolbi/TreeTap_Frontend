b59b2670e3aff93de3d709e7ffe514c8e080ef39
Yool Bi Lee <yoolbi716@gmail.com>
2023-03-03 21:54:14 -0500

    Initial setup and Start page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Start from "./pages/Start";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Company from "./pages/Company";
+import Profile from "./pages/Profile";
+
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Login" element={<Login />} />
+
+          <Route path="/Company" element={<Company />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </Router>
   );
 }
 
 export default App;
+
---
+++ src/pages/Start.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {Button} from "@mui/material";
+
+const Start = () => {
+
+    return (
+        <div style={{width: "100%", height: "100%"}}>
+            <div style={{width: "100%", height: "70px"}}>
+                <img
+                    src="/images/logo.png"
+                    style={{
+                        width: "55px",
+                        height: "55px",
+                        marginLeft: "100px",
+                        marginTop: "20px"
+                    }}/>
+            </div>
+            <div style={{width: "100%", height: "100%", display: "flex"}}>
+                <div style={{width: "45%", height: "100%", marginLeft: "150px", marginTop: "200px"}}>
+                    <div style={{fontFamily: "Open Sans", color: "#0D6F12", fontSize: "96px", fontWeight: "bold"}}>TreeTap</div>
+                    <div style={{fontFamily: "Sorts Mill Goudy", fontSize: "36px"}}>"Click for trees, make a change with ease!"</div>
+                    <div style={{fontFamily: "Open Sans", fontSize: "20px", marginTop: "5px"}}>TreeTap is a sustainable advertising platform that connects businesses with reforestation organizations to promote eco-friendly practices and encourage reforestation efforts.</div>
+                    <Button variant="contained" color="success" style={{marginTop: "20px"}}>
+                        START PLANTING
+                    </Button>
+                </div>
+                <img style={{height: "740px", width: "800px"}} src="/images/start_logo.png"/>
+            </div>
+            <div style={{marginLeft: "100px", marginBottom: "10px", fontFamily: "Open Sans", fontSize: "20px"}}>Our Partners</div>
+            <div style={{width: "100%", height: "100px", marginLeft: "100px"}}>
+                <img src="/images/partners.png"/>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Start;
