63d510bd42ffb80a6ad65b7a442691052d0bf611
Yongjung Shin <yongshn220@gmail.com>
2023-03-04 05:26:02 -0500

    Add | Animation and number Counter

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,38 +1,69 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Banner from "./Banner";
 
 
 
+const stateBarKeyStyle = {
+    display:'flex', alignItems:'center', justifyContent:'center', flex:1, fontSize:'15px', color:'#787878', fontWeight:'500', fontFamily: "Open Sans"
+}
 
+const stateBarValueStyle = {
+    display:'flex', alignItems:'end', justifyContent:'center', flex:1.5, fontSize:'25px', color:'#4D4D4D', fontWeight:'550', fontFamily: "Open Sans"
+}
 
+const couponList = [
+    { label: 'Coupon1'},
+    { label: 'Coupon2'},
+    { label: 'Coupon3'}]
+
+const CouponDropDown = () => {
+    return (
+        <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={couponList}
+            sx={{ width: 450 }}
+            renderInput={(params) => <TextField {...params} label="Coupons" />}
+        />
+    );
+}
 
 const Profile = () => {
-    const couponList = [
-        { label: 'Coupon1'},
-        { label: 'Coupon2'},
-        { label: 'Coupon3'}]
 
-    const CouponDropDown = () => {
-        return (
-            <Autocomplete
-                disablePortal
-                id="combo-box-demo"
-                options={couponList}
-                sx={{ width: 450 }}
-                renderInput={(params) => <TextField {...params} label="Coupons" />}
-            />
-        );
-    }
 
-    const stateBarKeyStyle = {
-        display:'flex', alignItems:'center', justifyContent:'center', flex:1, fontSize:'15px', color:'#787878', fontWeight:'500', fontFamily: "Open Sans"
-    }
+    let targetTreeCount = 1230;
+    let duration = 1000;
+    const [curTreeCount, setCount] = useState(0);
 
-    const stateBarValueStyle = {
-        display:'flex', alignItems:'end', justifyContent:'center', flex:1.5, fontSize:'25px', color:'#4D4D4D', fontWeight:'550', fontFamily: "Open Sans"
-    }
+    useEffect(() => {
+        let intervalId = null;
+        let startTime = null;
+        const startCount = curTreeCount;
+
+        const incrementCount = (timestamp) => {
+            if (!startTime) {
+                startTime = timestamp;
+            }
+
+            const elapsedTime = timestamp - startTime;
+            const progress = Math.min(elapsedTime / duration, 1);
+            const newCount = Math.floor(progress * (targetTreeCount - startCount)) + startCount;
+
+            setCount(newCount);
+
+            if (progress < 1) {
+                intervalId = requestAnimationFrame(incrementCount);
+            }
+        };
+
+        intervalId = requestAnimationFrame(incrementCount);
+
+        return () => {
+            cancelAnimationFrame(intervalId);
+        };
+    }, [targetTreeCount, duration]);
 
     return (
         <div style={{position:'absolute', display:'flex', flexFlow:"column", width:"100%", height:"100%"}}>
@@ -53,7 +84,7 @@ const Profile = () => {
                 <div style={{display:'flex', flex: '1'}}>
                     <div style={{display:'flex', flexFlow:'column', flex:'1'}}>
                         <div style={stateBarValueStyle}>
-                            1,340
+                            {curTreeCount.toLocaleString()}
                         </div>
                         <div style={stateBarKeyStyle}>
                             TREES
@@ -65,13 +96,13 @@ const Profile = () => {
                             <img style={{height: "28px", width: "28px"}} src="/images/level1.png"/>
                         </div>
                         <div style={stateBarKeyStyle}>
-                            LEVELS
+                            LEVEL
                         </div>
                         <div style={{flex: '0.3'}}></div>
                     </div>
                     <div style={{display:'flex', flexFlow:'column', flex:'1'}}>
                         <div style={stateBarValueStyle}>
-                            4690
+                            {(curTreeCount * 45).toLocaleString()}
                         </div>
                         <div style={stateBarKeyStyle}>
                             CO2
@@ -81,12 +112,27 @@ const Profile = () => {
                 </div>
                 <div style={{flex: '1'}}/>
             </div>
-            <div style={{display:'flex', flexFlow:'column', flex:'1 0 auto', backgroundColor:"white"}}>
+            <div style={{display:'flex', flexFlow:'column', flex:'1 0 auto', backgroundSize:'100% 100%', backgroundImage: `url("./images/profileBackground2.png")`}}>
                 <div style={{display:'flex', alignItems:'center', justifyContent:'center', flex:'0 0 130px'}}>
                     <CouponDropDown/>
                 </div>
                 <div style={{display:'flex', alignItems:'center', justifyContent:'center', flex:'0 0 400px', overflow:'hidden'}}>
-                    <img style={{height: "800px", width: "800px"}} src="/images/land22.png"/>
+                    <img style={{width:'500px', height:'auto', animation: 'floating 3s ease-in-out infinite'}} src="/images/land22.png"/>
+                    <style>
+                    {`
+                      @keyframes floating {
+                        0% {
+                          transform: translate(0, 0);
+                        }
+                        50% {
+                          transform: translate(0, -10px);
+                        }
+                        100% {
+                          transform: translate(0, 0);
+                        }
+                      }
+                    `}
+                    </style>
                 </div>
             </div>
         </div>
