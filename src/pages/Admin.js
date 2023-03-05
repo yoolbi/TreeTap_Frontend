f59fa88e944fbf55bc71aab73f09fdb4ae734b7b
Yongjung Shin <yongshn220@gmail.com>
2023-03-04 21:35:52 -0500

    Update name

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,6 +4,22 @@ import urlJoin from "url-join";
 import Banner from "./Banner";
 
 
+const loadAds = (data) => {
+    console.log(data);
+    if (!data) return;
+
+    let htmlElement = document.getElementById("adminHtml");
+    data.forEach((ad) => {
+        let item = `<div style={{flex: 1}}>{ad.company_name}</div>
+                    <div style={{flex: 1}}>{ad.website}</div>
+                    <div style={{flex: 1}}>{ad._id}</div>
+                    <div style={{flex: 1}}><input></input></div>
+                    <div style={{flex: 1}}><button>Approve</button></div>
+                </div>`
+        htmlElement.innerhtml += item;
+    })
+}
+
 const Admin = () => {
     const [ads, setAds] = useState([])
 
@@ -14,16 +30,27 @@ const Admin = () => {
                     urlJoin(process.env.REACT_APP_FRONTEND_URL, "/")
                 );
             } else if (data.status === 200) {
-                console.log(data);
+                setAds(data.body);
             }
         });
     }, [])
 
+    console.log(ads);
+    loadAds(ads);
+    let ad1 = ads[0];
     return (
         <div>
             <Banner/>
-            <div>
-                hello.
+            <div style={{display:'flex', flexFlow:'column', fontSize:'12px'}}>
+                <div style={{flex:'0 0 30px', display:'flex', alignItems:'center', justifyContent:'center',}}>
+                    <div style={{flex: 1}}>companyName</div>
+                    <div style={{flex: 1}}>website</div>
+                    <div style={{flex: 1}}>coupon</div>
+                    <div style={{flex: 1}}>NGO</div>
+                    <div style={{flex: 1}}>Approve</div>
+                </div>
+                <div style={{flex:'0 0 2px', backgroundColor:'black'}}></div>
+                <div id="adminHtml" style={{flex:'1', display:'flex', alignItems:'center', justifyContent:'center'}}></div>
             </div>
         </div>
     )
